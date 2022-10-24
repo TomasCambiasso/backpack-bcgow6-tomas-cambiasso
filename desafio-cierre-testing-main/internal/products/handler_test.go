43254f5c198a2fd6ddcdfe333d61f1f0122325a4
TomasCambiasso <tomas.cambiasso@mercadolibre.com>
2022-10-24 12:05:30 -0300

    feat: finish tests

--- desafio-cierre-testing-main/internal/products/handler_test.go
+++ desafio-cierre-testing-main/internal/products/handler_test.go
@@ -40,7 +40,7 @@ func buildProductsRoutes(r *gin.RouterGroup, db map[string][]Product) {
 }
 
 func TestGetProductsOk(t *testing.T) {
-
+	//arrange
 	db := make(map[string][]Product)
 	db["FEX112AC"] = append(db["FEX112AC"], Product{
 		ID:          "mock",
@@ -49,14 +49,86 @@ func TestGetProductsOk(t *testing.T) {
 		Price:       123.55,
 	})
 	r := createServer(db)
-	
-	req := httptest.NewRequest(http.MethodGet, "/products?seller_id=FEX112AC", nil)
+
+	//act
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/products?seller_id=FEX112AC", nil)
 	req.Header.Add("Content-Type", "application/json")
 	res := httptest.NewRecorder()
 	r.ServeHTTP(res, req)
 	var resProducts []Product
-	err := json.Unmarshal(res.Body.Bytes(), resProducts)
+	err := json.Unmarshal(res.Body.Bytes(), &resProducts)
+
+	//assert
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusOK, res.Code)
+
+	var expProducts []Product
+	expProducts = append(expProducts, Product{
+		ID:          "mock",
+		SellerID:    "FEX112AC",
+		Description: "generic product",
+		Price:       123.55,
+	})
+	assert.Equal(t, resProducts, expProducts)
+
+}
+
+func TestGetProductsNoID(t *testing.T) {
+	//arrange
+	db := make(map[string][]Product)
+	r := createServer(db)
+
+	//act
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/products", nil)
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	resJson := gin.H{}
+	err := json.Unmarshal(res.Body.Bytes(), &resJson)
+
+	//assert
+	assert.Nil(t, err)
+	assert.Equal(t, 400, res.Code)
+	assert.Equal(t, gin.H{"error": "seller_id query param is required"}, resJson)
+
+}
+
+func TestGetProductsI(t *testing.T) {
+	//arrange
+	db := make(map[string][]Product)
+	r := createServer(db)
+
+	//act
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/products", nil)
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	resJson := gin.H{}
+	err := json.Unmarshal(res.Body.Bytes(), &resJson)
+
+	//assert
+	assert.Nil(t, err)
+	assert.Equal(t, 400, res.Code)
+	assert.Equal(t, gin.H{"error": "seller_id query param is required"}, resJson)
+
+}
+
+func TestGetProductsIDNotFound(t *testing.T) {
+	//arrange
+	db := make(map[string][]Product)
+	r := createServer(db)
+
+	//act
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/products?seller_id=FEX112AC", nil)
+	req.Header.Add("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	resJson := gin.H{}
+	err := json.Unmarshal(res.Body.Bytes(), &resJson)
+
+	//assert
+	assert.Nil(t, err)
+	assert.Equal(t, 404, res.Code)
+	assert.Equal(t, gin.H{"error": "seller_id not found"}, resJson)
 
 }
--- desafio-cierre-testing-main/internal/products/service_test.go
+++ desafio-cierre-testing-main/internal/products/service_test.go
@@ -30,3 +30,19 @@ func TestGetAllBySellerOK(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expProducts, actualProducts)
 }
+
+func TestGetAllBySellerNotFound(t *testing.T) {
+
+	//assign
+	db := make(map[string][]Product)
+	repository := NewRepository(db)
+	service := NewService(repository)
+
+	//act
+	actualProducts, err := service.GetAllBySeller("FEX112AC")
+	var expProducts []Product
+
+	//assert 
+	assert.ErrorContains(t, err, "seller_id not found")
+	assert.Equal(t, expProducts, actualProducts)
+}
